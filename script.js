c0685e8190f57689ebb1c749f3bf4b11461c8932
ishayashika <122111684+ishayashika@users.noreply.github.com>
2024-12-09 19:14:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function daysOfYear(year){
+	let leapyear=(year%4===0 && year%100!==0) || (year%400===0)
+	return leapyear ? 366 : 365;
+}
+console.log(daysOfAYear(2022));
+console.log(daysOfAYear(2024));
+console.log(daysOfAYear(1900));
+console.log(daysOfAYear(2000));
+
